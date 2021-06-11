9b6c30b160981f073fbf21121e726ab91388e1fc
nikhil076 <nikhil.arvind7@gmail.com>
2021-06-11 10:56:30 +0530

    made changes in the headtail file

--- day6/day6pracs/while_headTails.sh
+++ day6/day6pracs/while_headTails.sh
@@ -1,15 +1,18 @@
 #!/bin/bash -x
 headcount=0
 tailcount=0
-while [ $headcount -lt 11 -o $tailcount -lt 11 ]
+while [ $headcount -lt 11 -a $tailcount -lt 11 ]
 do
 	random=$((RANDOM%2))
 	if [ $random -eq 0 ]
 	then
 		echo "heads"
-		headcount=`expr $headcount + 1`
+		headcount=$(($headcount+1))
 	else
 		echo "tails"
-		tailcount=`expr $tailcount + 1`
+		tailcount=$(($tailcount+1))
 	fi
 done
+
+echo "Head count is $headcount"
+echo "Tail count is $tailcount"
